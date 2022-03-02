8cf79818bfdeea2f5e1500b3aacbfc49da5e6a35
emchateau <emchateau@laposte.net>
2022-03-02 17:53:34 -0500

    ajout d’un générateur de site statique

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,4 +7,14 @@ const config = {
 	}
 };
 
-export default config;
+export default {
+	kit: {
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		})
+	}
+};
