dd4f453b432cc561e74e6447cd398ead21e8f853
emchateau <emchateau@laposte.net>
2022-03-17 11:55:01 -0400

    changement des chemins

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,9 @@
 <header class="navbar">
 	<section class="navbar-section">
 		<a href="../" class="navbar-brand mr-2">Réseaux sensibles</a>
-		<a href="/reseaux" class="btn btn-link">Redes</a>
-		<a href="/critiques" class="btn btn-link">Críticas de Arte</a>
-		<a href="/a-propos" class="btn btn-link">Sobre</a>
+		<a href="/rs/reseaux" class="btn btn-link">Redes</a>
+		<a href="/rs/critiques" class="btn btn-link">Críticas de Arte</a>
+		<a href="/rs/a-propos" class="btn btn-link">Sobre</a>
 	</section>
 </header>
 
