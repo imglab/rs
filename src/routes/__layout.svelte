ec324fccd74ad6cb234bd40b98eb91fae9f5b897
talitha.motter@gmail.com <talitha.motter@gmail.com>
2022-03-23 15:34:25 -0400

    test cdn

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,6 +4,11 @@
 		<a href="/reseaux" class="btn btn-link">Redes</a>
 		<a href="/critiques" class="btn btn-link">Críticas de Arte</a>
 		<a href="/a-propos" class="btn btn-link">Sobre</a>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.21.0/cytoscape.min.js" integrity="sha512-6NNH8uUbdENB10r5FH/4DZviHTQ44GtZ+c+fwSiWM4GekjajHkYanpH0+jWt3meFll/HHWAcxKLcMpSAXuT/Dg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.21.0/cytoscape.esm.js" integrity="sha512-RTfyIa3ZxryS01ObF38oKa0YH7JDbWiuGzwbygio4DumYOeSQJvY/lkRiPkYvsowMNM+BY38pb7Gl0J02r9Upw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.21.0/cytoscape.esm.min.js" integrity="sha512-QDeM03m31yKvsDS9c5y8pi+FmhFDLqQOBrv/mN5zrGMw7c94UzjF8TIRuYfEcbP3czCHOOq1tq0DJgzGDajGzA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.21.0/cytoscape.umd.js" integrity="sha512-xSF6S1H8UROpNDWZClp2akRXK6RhFiXvj+XrehPKfgzWjxYAON+AOMsbT0/UlahXiSVvGvkeafM8L8fZG/zjyw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/cytoscape/3.21.0/cytoscape.umd.min.js" integrity="sha512-43GqnpGcxI3IKbvlBnxyaKwTgTXbWT5Lmxm5Rs929uFBpxU05xfQy0Z+UIIBDmmf8y1lMEQAPUDqSP4bf3CiWw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 	</section>
 </header>
 
