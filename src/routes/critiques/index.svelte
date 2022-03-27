a980cce012e1d328e1b6081cea6707173655008e
talitha.motter@gmail.com <talitha.motter@gmail.com>
2022-03-27 18:11:38 -0400

    menu critiques

--- src/routes/critiques/index.svelte
+++ src/routes/critiques/index.svelte
@@ -13,9 +13,9 @@
 
 <h2>Críticas</h2>
 <ul>
-  <li><a href="/critiques/critiques_iconica" class="btn btn-link">Críticas publicadas pela Icônica</a></li>  
-  <li><a href="/critiques/critiques_performatus" class="btn btn-link">Críticas publicadas pela eRevista Performatus</a></li>
-  <li><a href="/critiques/critiques_arte_contexto" class="btn btn-link">Críticas publicadas pela Arte ConTexto</a></li>
-  <li><a href="/critiques/critiques_desvio" class="btn btn-link">Críticas publicadas pela Revista Desvio</a></li>
-  <li><a href="/critiques/critiques_ofermento" class="btn btn-link">Críticas publicadas pela o fermento</a></li>
+  <li><a href="/critiques/critiques_iconica" class="btn btn-link">Críticas de Arte - Icônica</a></li>  
+  <li><a href="/critiques/critiques_performatus" class="btn btn-link">Críticas de Arte - eRevista Performatus</a></li>
+  <li><a href="/critiques/critiques_arte_contexto" class="btn btn-link">Críticas de Arte - Arte ConTexto</a></li>
+  <li><a href="/critiques/critiques_desvio" class="btn btn-link">Críticas de Arte - Revista Desvio</a></li>
+  <li><a href="/critiques/critiques_ofermento" class="btn btn-link">Críticas de Arte - o fermento</a></li>
 </ul>
