81357e59a4a4e9fbf45cf87da66b56becc074b3b
talitha.motter@gmail.com <talitha.motter@gmail.com>
2022-03-27 17:15:00 -0400

    home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,7 @@
   <title>Réseaux sensibles - Corpus</title>
 </svelte:head>
 
-<div class="hero bg-gray hero-small">
+<div class="hero bg-secondary hero-sm">
 	<div class="hero-body">
 		<h1>Corpus Réseaux Sensibles</h1>
 		<p>Críticas de arte publicadas pela revistas digitais Icônica, eRevista Performatus, Arte ConTexto, Revista Desvio e o fermento até abril 2019</p>
@@ -11,50 +11,43 @@
 
 <p></p>
 
-<div>
-   <h3>Críticas de Arte</h3>
-   <ul>
-	  <li>Icônica: 73</li>
-	  <li>eRevista Performatus: 65</li>
-	  <li>Arte ConTexto: 42</li>
-	  <li>Revista Desvio: 97</li>
-	  <li>o fermento: 23</li>
-	  <li>Total de Textos: 300</li>
-	  <li>Total de Autores: 111</li>
-   </ul>
+<div class="container">
+	<div class="columns">
+		<div class="column col-2"></div>
+		<div class="column col-3">
+				<h3>Críticas de Arte</h3>
+				<ul>
+					<li>Icônica: 73</li>
+	  				<li>eRevista Performatus: 65</li>
+	  				<li>Arte ConTexto: 42</li>
+	  				<li>Revista Desvio: 97</li>
+	  				<li>o fermento: 23</li>
+	  				<li>Total de Textos: 300</li>
+	  				<li>Total de Autores: 111</li>
+   				</ul>
+		</div>
+		<div class="column col-3">
+				<h3>Pessoas</h3>
+				<ul>
+				   <li>Artistas: 1069</li>
+				   <li>Coletivos de Artistas: 78</li>
+				   <li>Curadores: 121</li>
+				   <li>Outras Pessoas: 221</li>
+				   <li>Outros Coletivos: 2</li>
+				</ul>
+		</div>
+		<div class="column col-4">
+				
+   				<h3>Produções artísticas</h3>
+   				<ul>
+					<li>Total de Obras e Séries de Obras: 944</li>
+					<li>Total de Eventos Artísticos: 326</li>
+   				</ul>
+   				<h3>Lugares de Exposição</h3>
+  				<ul>
+					<li>Total de Lugares: 218</li>
+   				</ul>
+		</div>
+	</div>	
 </div>
-<div>
-   <h3>Pessoas Citadas</h3>
-   <ul>
-	  <li>Artistas: 1069</li>
-	  <li>Coletivos de Artistas: 78</li>
-	  <li>Curadores: 
-		 121
-	  </li>
-	  <li>Outras Pessoas: 
-		 221
-	  </li>
-	  <li>Outros Coletivos: 
-		 2
-	  </li>
-   </ul>
-</div>
-<div>
-   <h3>Obras e Séries de Obras Citadas</h3>
-   <ul>
-	  <li>Total de Obras e Séries de Obras: 944</li>
-   </ul>
-</div>
-<div>
-   <h3>Exposições de Arte Citadas</h3>
-   <ul>
-	  <li>Total de Exposições: 326</li>
-   </ul>
-</div>
-<div>
-   <h3>Lugares de Exposição Citados</h3>
-   <ul>
-	  <li>Total de Lugares: 218</li>
-   </ul>
-</div>
-<div><sub>2022-03-08T22:23:05.666-05:00</sub></div>
+<div></div>
