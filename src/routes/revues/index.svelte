2731d46a2d6cec1fbef179e622f3a93d2c7ff81b
talitha.motter@gmail.com <talitha.motter@gmail.com>
2024-01-02 16:32:53 -0500

    ajout de la chronologie dans la page des revues

--- src/routes/revues/index.svelte
+++ src/routes/revues/index.svelte
@@ -16,14 +16,19 @@
 <div class="container">
 	<div class="columns">
 		<div class="column col-6">
-			<p>Com o objetivo de melhor entender o universo das revistas digitais que abordam o tema da arte no Brasil, durante a minha pesquisa de doutorado, eu realizei um levantamento dessas publicações de modo a identificar suas características gerais. Nesse levantamento, informações que permitem o referenciamento bibliográfico dessas publicações - nome, site web, se a revista ainda está disponível ou não, data da primeira e última publicação consultada, cidade e estado - foram incluídas. Além disso, eu adicionei informações sobre o conteúdo dessas revistas, por exemplo, se elas publicam ou não em formato PDF ou impresso, e sobre os meios de divulgação. Também inclui os temas gerais de cada revista, identificados nos textos de apresentação e em uma consultação rápida dos conteúdos publicados. Os meios de financiamento identificados dessas revistas também foram incluídos, de maneira não exaustiva, no levantamento, além de outras atividades que essas publicações realizaram, como exposições e oficinas.</p>
-            <p>Nesse levantamento, que foi encerrado em agosto 2023, não foram incluídas as revistas digitais com gestão vinculada a universidades, de maneira a reduzir o escopo das revistas levantadas, focando em iniciativas que tentam alcançar outros públicos.</p>
+			<p>Com o objetivo de melhor entender o universo das revistas digitais que abordam o tema da arte no Brasil, eu realizei um <a href="http://localhost:3000/revues/revues_repertoire">levantamento</a> dessas publicações de modo a identificar suas características gerais. Nesse levantamento, informações que permitem o referenciamento bibliográfico das revistas - nome, site web, se a revista ainda está disponível ou não, data da primeira e última publicação consultada, cidade e estado - foram incluídas. Além disso, eu adicionei informações sobre o conteúdo dessas revistas, por exemplo, se elas publicam ou não em formato PDF ou impresso, e sobre os meios utilizados para a divulgação de seus conteúdos. Também inclui os temas gerais abordados por cada revista, identificados nos textos de apresentação e em uma consultação rápida dos conteúdos publicados. Os meios de financiamento identificados também foram inseridos no levantamento de maneira não exaustiva, além de outros tipos de atividades realizadas por essas publicações, como exposições e oficinas.</p>
+            <p>Nesse levantamento, que foi encerrado em agosto de 2023, não foram incluídas as revistas digitais com gestão universitária, de maneira a reduzir o escopo das revistas levantadas, focando em iniciativas que tentam alcançar outros públicos.</p>
+			<p>Com os dados levantados, realizei o exercício de criar uma <a href="https://bit.ly/reseaux-sensibles-chronologie">cronologia</a>, a partir da ferramenta open source <a href="https://timeline.knightlab.com/">TimelineJS</a>, permitindo a visualização interativa do período que compreende as datas da primeira e da última publicação identificada de cada revista.</p>
 		</div>
 		<div class="column col-1"></div>
 		<div class="column col-5">
-			<h4>Acesse a tabela de revistas - em francês</h4>
+			<h4>Acesse a tabela de revistas</h4>
 			<ul>
-				<li><a href="/revues/revues_repertoire" class="btn btn-link">Revistas</a></li>
+				<li><a href="/revues/revues_repertoire" class="btn btn-link">Revistas - en francês</a></li>
+			</ul>
+			<h4>Acesse a cronologia</h4>
+			<ul>
+				<li><a href="https://bit.ly/reseaux-sensibles-chronologie" class="btn btn-link">Cronologia - en francês</a></li>
 			</ul>
 		</div>
 	</div>
