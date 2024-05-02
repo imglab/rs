95df078e6c24c8d3d2ec1dcdf010ecd16cbe7c74
talitha.motter@gmail.com <talitha.motter@gmail.com>
2024-05-02 16:59:45 -0400

    mise à jour textes

--- src/routes/reseaux/reseauxBimodaux.svelte
+++ src/routes/reseaux/reseauxBimodaux.svelte
@@ -21,8 +21,8 @@
  <div class="container">
      <div class="columns">
          <div class="column col-6">
-             <p>As redes bimodais aqui presentes foram criadas a partir dos artitas-temas das críticas de arte de cada uma das cinco revistas do corpus e dos autores que escreveram sobre eles. Assim, uma crítica de arte que trata de uma exposição coletiva gera a conexão de seu autor com os nomes dos artistas mencionados em seu texto e que tiveram presentes na mostra. No caso de um autor que escreveu várias críticas, seu nome sera vinculado com o de todos os seus artistas-tema.</p>
-             <p>As cores dos nós correspondem ao país de nascimento dos artistas-tema e dos autores, em azul os  nascidos no Brasil e em cinza-claro aqueles sobre os quais não foi possível identificar essa informação. Os autores são representados pela forma de losangos e os artistas-tema pela forma de elipses. Para a criação das redes não foram consideradas as críticas escritas exclusivamente sobre artistas coletivos.</p>
+             <p>As redes bimodais aqui presentes foram criadas a partir dos(as) artitas-temas das críticas de arte de cada uma das cinco revistas do corpus e dos(as) autores(as) que escreveram sobre eles(elas). Assim, uma crítica de arte que trata de uma exposição coletiva gera a conexão de seu(sua) autor(a) com os nomes dos(as) artistas mencionados em seu texto e que tiveram presentes na mostra. No caso de um(a) autor(a) que escreveu várias críticas, seu nome será vinculado com o de todos os(as) seus(suas) artistas-tema.</p>
+             <p>As cores dos nós correspondem ao país de nascimento dos(as) artistas-tema e dos(as) autores(as), em azul os(as) nascidos(as) no Brasil e em cinza aqueles(as) sobre os quais não foi possível identificar essa informação. Os(as) autores(as) são representados pela forma de losangos e os(as) artistas-tema pela forma de elipses. Para a criação das redes não foram consideradas as críticas escritas exclusivamente sobre coletivos de artistas que não mencionam nomes de artistas individualmente.</p>
          </div>
          <div class="column col-1"></div>
          <div class="column col-5">
--- src/routes/reseaux/reseauxUnimodauxEIC.svelte
+++ src/routes/reseaux/reseauxUnimodauxEIC.svelte
@@ -24,9 +24,9 @@
 <div class="container">
 	<div class="columns">
 		<div class="column col-6">
-			<p>Aqui você pode acessar uma segunda versão das redes unimodais formadas pelos artistas-tema de cada revista que mostra o resultado da pesquisa de seus nomes na <a href="https://enciclopedia.itaucultural.org.br/">Enciclopédia Itaú Cultural</a> - EIC. A ideia foi de estabecer um paralelo entre a identificação de artistas realizada por essas cinco publicações e esta importante referência digital, que realiza o trabalho de repertoriar atores que atuam ou atuaram para o desenvolvimento do mundo da arte brasileiro.</p>
-            <p>A cor verde dos nós representa que o artista foi identificado na busca da EIC, a cor amarela mostra os artistas que não foram localizados na enciclopédia.</p>
-            <p>Este é um resultado aproximativo, pois os dados da EIC evoluem com o tempo e informações complementares faltantes sobre os artistas podem vir a contribuir com a localização dos mesmos na base de dados da enciclopédia.</p>
+			<p>Aqui você pode acessar uma segunda versão das redes unimodais formadas pelos artistas-tema de cada revista que mostra o resultado da pesquisa de seus nomes na <a href="https://enciclopedia.itaucultural.org.br/">Enciclopédia Itaú Cultural</a> - EIC. A ideia foi de estabecer um paralelo entre a identificação de artistas realizada por essas cinco publicações e esta importante referência digital no Brasil, que realiza o trabalho de repertoriar atores(as) que atuam ou atuaram para o desenvolvimento do mundo da arte brasileiro.</p>
+            <p>A cor verde dos nós representa que o(a) artista foi identificado na busca da EIC, a cor amarela mostra os(as) artistas que não foram localizados na enciclopédia.</p>
+            <p>Este é um resultado aproximativo, pois informações faltantes sobre os(as) artistas podem dificultar a localização na base de dados da enciclopédia e os dados da EIC evoluem com o tempo.</p>
 		</div>
 		<div class="column col-1"></div>
 		<div class="column col-5">
